333eb16d59f9cc3d0a973e4fe35aeaf140b3d9d4
Daniela van der Heijden <info@wwwebsites.com>
2010-08-16 16:56:06 +0100

    2.3.8?

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 # and I hope this is what I have achieved when rake rails:update after setting it here
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
